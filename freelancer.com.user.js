8d9aebc5de46360b1b76dea9ad4bab86c42aa144
Sazzad Hossain Khan <itsazzad@gmail.com>
2021-08-01 20:32:02 +0600

    await async + bid stats

--- freelancer.com.user.js
+++ freelancer.com.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  freelancer.com
 // @author       Sazzad Hossain (Tushar) Khan <itsazzad@gmail.com>
-// @match        https://www.freelancer.com/projects/*/*/details
+// @match        https://www.freelancer.com/projects/*
 // @icon         https://www.google.com/s2/favicons?domain=freelancer.com
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
@@ -13,8 +13,89 @@
 (function ($) {
     'use strict';
 
-    function fl_list_item(key, value) {
-        return `<fl-list-item role="listitem" title="${key}">
+    const BASE_URL = `https://www.freelancer.com/api`;
+    let ProjectSeoUrl;
+    let ProjectID;
+    let Project;
+    let OwnerID;
+    let Owner;
+
+    addCSS();
+
+    $(document).on('click', "app-project-view-details .CardHeader:contains('Project Details')", async function () {
+        if (!Project) {
+            Project = await getProject();
+        }
+        console.log(Project)
+        showBidStats(Project.result.projects[0].bid_stats.bid_count);
+    });
+
+    $(document).on('click', 'app-employer-info .CardHeader', async function () {
+        if (!Project)
+            Project = await getProject();
+
+        if (!OwnerID)
+            OwnerID = Project.result.projects[0].owner_id;
+
+        if (!Owner) {
+            Owner = await getOwner(OwnerID);
+        }
+
+        updateEmployerInfo(Owner.result.users[OwnerID]);
+    });
+
+    function showBidStats(count) {
+        $("app-project-view-tabs .TabList fl-tab-item:contains('Proposals') a").append(bidStatsBadge(count));
+    }
+
+    function updateEmployerInfo(employer) {
+        const appEmployerInfoList$ = $('app-employer-info .CardBody fl-list-item:eq(0) fl-list');
+        employer.username && appEmployerInfoList$.append(employerInfoListItem("Username", employer.username));
+        employer.display_name && appEmployerInfoList$.append(employerInfoListItem("Display Name", employer.display_name));
+        employer.public_name && appEmployerInfoList$.append(employerInfoListItem("Public Name", employer.public_name));
+        employer.company && appEmployerInfoList$.append(employerInfoListItem("Company", employer.company));
+    }
+
+    async function doAjax(url) {
+        let result;
+
+        try {
+            result = await $.ajax({
+                url,
+            });
+
+            console.log("Ajax", result);
+
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function getOwner(userID) {
+        const url = `${BASE_URL}/users/0.1/users?users[]=${userID}`;
+
+        return await doAjax(url);
+    }
+
+    async function getProject() {
+        if (!ProjectSeoUrl)
+            ProjectSeoUrl = window.location.pathname.replace(/^\/projects\//, "").replace(/\/details$/, "");
+
+        let url;
+        if (ProjectSeoUrl) {
+            url = `${BASE_URL}/projects/0.1/projects?seo_urls[]=${ProjectSeoUrl}`;
+        } else {
+            if (!ProjectID)
+                ProjectID = $('.ProjectViewDetailsId').text().trim().match(/\d+/g)[0];
+            url = `${BASE_URL}/projects/0.1/projects?projects[]=${ProjectID}`;
+        }
+
+        return await doAjax(url);
+    }
+
+    function employerInfoListItem(key, value) {
+        return `<fl-list-item role="listitem" title="${key}" class="user-script-employer-info-list-item">
         <fl-bit class="BitsListItem">
             <fl-bit class="BitsListItemContainer">
                 <fl-bit class="BitsListItemHeader">
@@ -28,40 +109,38 @@
         </fl-bit>
     </fl-list-item>`;
     }
-    $(document).on('click', 'app-employer-info .CardHeader', function () {
-        const BASE_URL = `https://www.freelancer.com/api`;
-        const projectID = $('.ProjectViewDetailsId').text().trim().match(/\d+/g)[0];
-        const seoURL = window.location.pathname.replace(/^\/projects\//, "").replace(/\/details$/, "");
 
-        let projectGetURL;
-        if (projectID) {
-            projectGetURL = `${BASE_URL}/projects/0.1/projects?projects[]=${projectID}`;
-        } else {
-            projectGetURL = `${BASE_URL}/projects/0.1/projects?seo_urls[]=${seoURL}`;
+    function bidStatsBadge(count) {
+        return `<fl-bit class="IconContainer user-script-bid-stats-badge">
+        <fl-text class="UnreadCount">
+            <span class="NativeElement">${count}</span>
+        </fl-text>
+    </fl-bit>`;
+    }
+
+    function addCSS() {
+        const style = `<style>
+        .user-script-employer-info-list-item {
+            font-size: 14px;
+            line-height: 1.43;
+            color: #0e1724;
+            font-weight: 400;
+            font-style: normal;
+            display: inline;
         }
-        $.ajax(projectGetURL)
-            .done(function (data, textStatus, jqXHR) {
-                console.log(data);
-                const userID = data.result.projects[0].owner_id;
-                $.ajax(`${BASE_URL}/users/0.1/users?users[]=${userID}`)
-                    .done(function (data, textStatus, jqXHR) {
-                        console.log(data);
-                        const userData = data.result.users[userID];
-                        const appEmployerInfoList$ = $('app-employer-info .CardBody fl-list-item:eq(0) fl-list');
-                        appEmployerInfoList$.append(fl_list_item("Username", userData.username));
-                        appEmployerInfoList$.append(fl_list_item("Display Name", userData.display_name));
-                        appEmployerInfoList$.append(fl_list_item("Public Name", userData.public_name));
-                        appEmployerInfoList$.append(fl_list_item("Company", userData.company));
-                    })
-                    .fail(function (jqXHR, textStatus, errorThrown) {
-                        console.error("Request failed: " + textStatus);
-                    });
+        
+        .user-script-bid-stats-badge .UnreadCount{
+            align-items: center;
+            background: #eb3730;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            height: 23px;
+            width: 23px;
+        }
+    </style>`;
 
-            })
-            .fail(function (jqXHR, textStatus, errorThrown) {
-                console.error("Request failed: " + textStatus);
-            });
-
-    });
+        $(style).appendTo('body');
+    }
 
 })($);
