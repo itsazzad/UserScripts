bd66f83bfa168e40e9a8c7dd80f7c7b92ca00696
Sazzad Hossain (Tushar) Khan <itsazzad@gmail.com>
2022-02-16 09:48:57 +0600

    freelancer messages objectify

--- freelancer.com.messages.user.js
+++ freelancer.com.messages.user.js
@@ -10,25 +10,42 @@
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // ==/UserScript==
 
+const ProjectsByDateTime = {};
 (function ($) {
     'use strict';
 
     addCSS();
 
     $(document).on('click', 'app-messaging-thread-list fl-user-avatar', function () {
+        console.log(0, ProjectsByDateTime)
         const messageList = document.getElementsByClassName('MessageList');
         const messageItems = messageList[0].getElementsByTagName('app-messaging-message-item');
-
+        let date = "";
+        let time = "";
         for (let messageItem of messageItems) {
             const messageHeaderText = messageItem.querySelector('[class^="Message-headerText"]');
-            if (messageHeaderText) {
-                console.log(0, messageHeaderText.textContent)
+            if (messageHeaderText) {//Date
+                date = messageHeaderText.textContent.trim();
+                if (!ProjectsByDateTime[date]) {
+                    ProjectsByDateTime[date] = {};
+                }
+
             }
-            const message = messageItem.getElementsByClassName('Message');
-            if (message.length) {
-                console.log(1, message[0].textContent)
+            const appMessagingMessageDetails = messageItem.getElementsByTagName('app-messaging-message-details');
+            if (appMessagingMessageDetails.length) {//Time
+                time = appMessagingMessageDetails[0].textContent.trim();
+                if (!ProjectsByDateTime[date][time])
+                    ProjectsByDateTime[date][time] = []
             }
+
+            const messageBubbleText = messageItem.getElementsByClassName('Message-bubble-text');
+            if (messageBubbleText.length) {//Details
+                let details = messageBubbleText[0].textContent.trim();
+                ProjectsByDateTime[date][time].push(details);
+            }
+
         }
+        console.log(1, ProjectsByDateTime);
     });
 
     function addCSS() {
