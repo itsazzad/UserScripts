ea8853ab20d4f23010a97082b5603e62e51863e0
Sazzad Hossain (Tushar) Khan <itsazzad@gmail.com>
2022-02-16 08:52:14 +0600

    Create freelancer.com.messages.user.js

---
+++ freelancer.com.messages.user.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         freelancer.com
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  freelancer.com
+// @author       Sazzad Hossain (Tushar) Khan <itsazzad@gmail.com>
+// @match        https://www.freelancer.com/messages/*
+// @icon         https://www.google.com/s2/favicons?domain=freelancer.com
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// ==/UserScript==
+
+(function ($) {
+    'use strict';
+
+    addCSS();
+
+    $(document).on('click', 'app-messaging-thread-list fl-user-avatar', function () {
+      const messageList = document.getElementsByClassName('MessageList');
+      const messageItems = messageList[0].getElementsByTagName('app-messaging-message-item');
+
+      for (let messageItem of messageItems) {
+          const messageHeaderText = messageItem.querySelector('[class^="Message-headerText"]');
+          if(messageHeaderText){
+              console.log(2, messageHeaderText.textContent)
+          }
+          const message = messageItem.getElementsByClassName('Message');
+          if(message.length){
+              console.log(4, message[0].textContent)
+          }
+      }
+    });
+
+    function addCSS() {
+        const style = `<style>
+    </style>`;
+
+        $(style).appendTo('body');
+    }
+
+})($);
